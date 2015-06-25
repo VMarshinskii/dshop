2bda9fb3c0dd881a301160c6daee547f6fd13c16
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-06-25 10:42:56 +0500

    Update

--- pages/models.py
+++ pages/models.py
@@ -25,7 +25,7 @@ class Page(models.Model):
     description = models.CharField("Описание", max_length=200)
     keyword = models.CharField("Ключевые слова", max_length=200)
     # section = models.ForeignKey(Section, blank=True, default="-1", help_text="Раздел", null=True, verbose_name="Раздел")
-    text = RedactorField(verbose_name="Текст", redactor_options={'lang': 'ru'})
+    text = RedactorField(verbose_name="Текст")
     url = models.CharField("Url", max_length=200)
 
     class Meta:
