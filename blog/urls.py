2c2b7220cda0365e7d8416e0451b91c8b44ed0a9
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-07-04 14:44:06 +0500

    Update

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^(?P<url>[\-\w]+)', 'pages.views.page_view'),
+    url(r'^', 'blog.views.posts_view'),
+    url(r'^(?P<url>[\-\w]+)/', 'pages.views.page_view'),
 )
