8c2d4573bca42641891020656e2a0c3b1217f5b2
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-07-11 13:27:39 +0500

    Update

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,7 @@ def posts_view(request):
 def post_view(request, id=-1):
     try:
         post = Post.objects.get(id=int(id))
+        print post.title
         return render_to_response("post_view.html", {'post', post})
     except Post.DoesNotExist:
         raise Http404
