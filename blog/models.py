0675bd15157aab8b31c83c431da37501489e7fae
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-07-11 18:50:32 +0500

    Update

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField("Название", max_length=200)
     description = models.CharField("Описание", max_length=200)
     keyword = models.CharField("Ключевые слова", max_length=200)
-    text = RedactorField(verbose_name="Текст", redactor_options={'upload_to': 'static/uploads', 'plugins': ['fontcolor']})
+    text = RedactorField(verbose_name="Текст", redactor_options={'upload_to': 'static/uploads'})
     url = models.CharField("Url", max_length=200)
     send = models.BooleanField(verbose_name="Разослать подписчикам", default=False)
 
