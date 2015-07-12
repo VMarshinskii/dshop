77a53b4cce328c79f23f5ed21279719433f31f0f
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-07-12 12:24:31 +0500

    Update

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,11 @@ from redactor.fields import RedactorField
 
 class Post(models.Model):
     title = models.CharField("Название", max_length=200)
-    description = models.CharField("Описание", max_length=200)
-    keyword = models.CharField("Ключевые слова", max_length=200)
+    description = models.CharField("Описание", max_length=200, blank=True)
+    keyword = models.CharField("Ключевые слова", max_length=200, blank=True)
     text = RedactorField(verbose_name="Текст", redactor_options={'upload_to': 'static/uploads'})
-    url = models.CharField("Url", max_length=200)
+    url = models.CharField("Url", max_length=200, unique=True)
+    lookbook = models.BooleanField(verbose_name="Публиковать в LookBook", default=False)
     send = models.BooleanField(verbose_name="Разослать подписчикам", default=False)
 
     class Meta:
