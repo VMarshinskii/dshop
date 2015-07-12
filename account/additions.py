3207b26b5692535a0188e8a0683a977f964981a4
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-07-12 18:17:59 +0500

    Update

--- account/additions.py
+++ account/additions.py
@@ -10,18 +10,23 @@ def registration_valid(request):
     password_again = request.POST['password_again']
 
     if first_name == '':
-        errors['first_name_error'] = "обязательное поле"
+        errors['first_name_error'] = "error_field"
+        errors['user_error'] = "Заполните обязательные поля"
     if email == '':
-        errors['email_error'] = "обязательное поле"
+        errors['email_error'] = "error_field"
+        errors['user_error'] = "Заполните обязательные поля"
     else:
         try:
             user = User.objects.get(email=email)
             errors['email_error'] = "пользователь с таким email уже существует"
+            errors['email_field_error'] = "error_field"
         except User.DoesNotExist:
             pass
     if password == '':
-        errors['password_error'] = "обязательное поле"
+        errors['password_error'] = "Обязательное поле"
+        errors['password_field_error'] = "error_field"
     if password != password_again:
-        errors['password_error'] = "пароли не совпадают"
+        errors['password_error'] = "Пароли не совпадают"
+        errors['password_field_error'] = "error_field"
 
     return errors
