945471956830a20a28a44777281de4c91b2432f7
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-12-02 17:51:02 +0500

    Добавлено поле метро в заказе Отправление оповещения клиенту при смене статуса заказа Номер телефона в письме

--- orders/forms.py
+++ orders/forms.py
@@ -6,7 +6,7 @@ from models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ['first_name', 'last_name', 'email', 'phone', 'index', 'address']
+        fields = ['first_name', 'last_name', 'email', 'phone', 'index', 'address', 'metro']
 
     def clean(self):
         cleaned_data = super(OrderForm, self).clean()
