e9c0bdf4d33fdcf2f9a5d0295815a16d57a09f6d
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-12-02 23:49:25 +0500

    Update

--- catalog/models.py
+++ catalog/models.py
@@ -138,7 +138,7 @@ class Product(models.Model):
 
 
 class ProductVideo(models.Model):
-    product = models.ForeignKey(Product, verbose_name="Товар")
+    product = models.ForeignKey(Product, verbose_name="Товар", null=True)
     code = models.TextField("Код видео", null=True)
     video = models.FileField(verbose_name="Видео файл", upload_to="/static/video/", null=True)
 
