e33ff11bbe22b7b1a9fe5962395e68494a406642
VMarshinskii <on_VIP5marshinskii@gmail.com>
2015-12-27 21:51:23 +0500

    Update

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from django.contrib import admin
 from models import Order, DeliveryType, OrderPhone
 
@@ -5,6 +6,10 @@ from models import Order, DeliveryType, OrderPhone
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ['id']
     list_display = ('id', 'last_name', 'first_name', 'status', 'date_now')
+
+    fields = ['id', 'user', 'sum', 'status', 'first_name', 'patronymic', 'last_name', 'email', 'phone', 'region',
+              'city', 'index', 'address', 'metro', 'delivery', 'delivery_price', 'products', 'admin_comment']
+
 
 
 class OrderPhoneAdmin(admin.ModelAdmin):
