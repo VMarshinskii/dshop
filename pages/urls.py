28728926c58452cdde5c4c53494c07eb833de37f
Marshinskii <marshinskii@gmail.com>
2016-04-20 22:27:32 +0500

    Создана view для поиска fix

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^/search/', 'pages.views.search_view'),
+    url(r'^search/', 'pages.views.search_view'),
     url(r'^(?P<url>[\-\w]+)', 'pages.views.page_view'),
 )
